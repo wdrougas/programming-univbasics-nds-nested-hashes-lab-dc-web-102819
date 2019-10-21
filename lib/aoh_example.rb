e0074c939765f999a8ae8542f7ce4d0d22e68c1b
Will Drougas <wdrougas@gmail.com>
2019-10-21 13:05:59 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -1,28 +1,30 @@
-# Examples inspired by David Foster Wallace's # (2/21/1962 to 9/12/2008) _Infinite Jest_
+# # # Examples inspired by David Foster Wallace's # (2/21/1962 to 9/12/2008) _Infinite Jest_
 
-DON_G = { name:  "Don Gately", occupation: "Live-in Staff" }
-JOELLE_VD = { name:  "Joelle van Dyne", occupation: "Radio Personality" }
-PAT_M =  { name:  "Pat Monteseian", occupation: "Staff" }
-KATE_G = { name:  "Kate Gompert", occupation: "None" }
-BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
+# DON_G = { name:  "Don Gately", occupation: "Live-in Staff" }
+# JOELLE_VD = { name:  "Joelle van Dyne", occupation: "Radio Personality" }
+# PAT_M =  { name:  "Pat Monteseian", occupation: "Staff" }
+# KATE_G = { name:  "Kate Gompert", occupation: "None" }
+# BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
 
 def assembled_aoh
-  [DON_G, JOELLE_VD, PAT_M, KATE_G, BRUCE_G]
+   [DON_G, JOELLE_VD, PAT_M, KATE_G, BRUCE_G]
 end
 
-def literal_aoh
-  [{ name:  "Don Gately", occupation: "Live-in Staff" },
+ def literal_aoh
+   [{ name:  "Don Gately", occupation: "Live-in Staff" },
    { name:  "Joelle van Dyne", occupation: "Radio Personality" },
    { name:  "Pat Monteseian", occupation: "Staff" },
    { name:  "Kate Gompert", occupation: "None" },
    { name:  "Bruce Green", occupation: "Fan of Mildred"}]
-end
+ end
 
 def aoh_lookup(aoh, row, key)
-  aoh[row][key]
+  aoh[row][key] 
 end
 
 def aoh_update(aoh, row, key, new_value)
-  aoh[row][key]=new_value
-  aoh
+   aoh[row][key]=new_value
+   aoh
 end
+
+
